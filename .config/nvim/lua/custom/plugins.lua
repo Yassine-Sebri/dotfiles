1f0c6bbe3f094242a4e85f75eb2dfc2020b2e210
Yassine Sebri <ysebri@mantu.com>
2024-02-15 10:01:41 +0100

    first commit

---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,70 @@
+local plugins = {
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+        "pyright",
+        "terraformls",
+        "yamlls",
+        "tflint",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+      require("core.utils").load_mappings("gopher")
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        mode = "background", -- You can change the mode to "foreground" to colorize foreground color codes
+        RGB = true, -- Enable RGB color notation support
+        RRGGBB = true, -- Enable RRGGBB color notation support
+        names = false, -- Disable color name highlighting (e.g., "red", "blue", etc.)
+        css = true, -- Disable CSS color notation support      
+      }
+    }
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end,
+  },
+  {
+    "tpope/vim-obsession",
+    lazy = false,
+  },
+}
+return plugins
---
+++ README.md
@@ -0,0 +1,18 @@
+# My dotfiles
+
+This repository contains the dotfiles for my system.
+
+## Requirements
+
+- Git
+- Zsh
+- [Oh My Zsh](https://github.com/ohmyzsh/ohmyzsh)
+- [powerlevel10k](https://github.com/romkatv/powerlevel10k)
+- [tmux](https://github.com/tmux/tmux)
+  - [tpm](https://github.com/tmux-plugins/tpm)
+- [Neovim](https://github.com/neovim/neovim)
+
+## Additional tools
+
+- [GitUI](https://github.com/extrawurst/gitui)
+- [K9S](https://github.com/derailed/k9s)
